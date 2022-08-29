351bdd242ed850833337210eb9e6b641d97d6ad7
Travis Williams <traviswilliams997@gmail.com>
2022-08-29 00:01:01 -0500

    Edit scraper function to place job information in hash

--- scraper.rb
+++ scraper.rb
@@ -7,7 +7,20 @@ def scraper
     unparsed_page = HTTParty.get(url)
     parsed_page = Nokogiri::HTML(unparsed_page)
     
- 
+    job_titles = parsed_page.css('div.block-content').css('h2')
+    job_info = parsed_page.css('div.entry-category')
+    job_titles.each_with_index do |job_title, index|
+        job = {
+            title: job_title.css('a')[0].attributes["title"].value,
+            information: job_info[index].css('p').text,
+            url: job_title.css('a')[0].attributes["href"].value
+
+
+        }
+        byebug
+    end 
+    
      
 
-end    
+end   
+scraper 
